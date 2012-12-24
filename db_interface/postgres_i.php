ecbf5911351f392ea3ae2318944abbb7ffd52805
Kujimichi Tatsuya <tatsu-kanaria@nifty.com>
2012-12-25 01:57:55 +0900

    PostgreSQL interface一応完成 （ブレインストーミング的に書いたので、エラー絶対出るコード。事前コンパイル必須、絶対動かすな。）

--- db_interface/postgres_i.php
+++ db_interface/postgres_i.php
@@ -1,6 +1,8 @@
 <?php
 /*
 	PostgreSQL Interface
+		interface：５行目〜
+		class本文：７０行目〜
 */
 interface db_connector
 {
@@ -21,6 +23,13 @@ interface db_connector
 	戻り値：正常完了：true, 異常終了：false
 	*/
 	function setbook(array $book);
+	
+	/*
+	本の冊数をアップデートする
+	引数：ISBN
+	戻り値：正常完了；true, 異常終了：false
+	*/
+	function addbook($isbn);
 	
 	/*
 	データベースから本の情報を取り出す
@@ -61,11 +70,11 @@ interface db_connector
 class postgres_i implements db_connector
 {
 	private $dbconn_book;
-	private $dbcon_borrow;
+	private $dbconn_borrow;
 	
 	function db_connect(){
-		$dbconn_book = pg_connect("dbname=book");
-		$dbconn_borrow = pg_connect("dbname=borrow");
+		$dbconn_book = pg_connect("dbname=bookshelf");
+		$dbconn_borrow = pg_connect("dbname=borrows");
 		if(! $dbconn_book | $dbconn_borrow){
 			return false;
 		}else{
@@ -74,13 +83,22 @@ class postgres_i implements db_connector
 	}
 	
 	function create_table(){
-		$query = "CREATE TABLE";
-		$query .= "";
+		$query = "CREATE TABLE bookshelf(";
+		$query .= "ISBN varchar(13) PRIMARY KEY,";
+		$query .= "URL varchar(500),";
+		$query .= "Image varchar(100),";
+		$query .= "Author varchar(100),";
+		$query .= "PubDate varchar(10),";
+		$query .= "Title varchar(100),";
+		$query .= "amount int);";
 		pg_query($dbconn_book, $query);
 		$error = pg_last_error($dbconn_book)."\n\n";
 		
-		$query = "";
-		$query .= "";
+		$query = "CREATE TABLE borrows(";
+		$query .= "ID varchar(20),";
+		$query .= "ISBN varchar(13) UNIQUE,";
+		$query .= "bDate date,";
+		$query .= "rDate date);";
 		pg_query($dbconn_borrow, $query);
 		$error .= pg_last_error($dbconn_borrow)."\n";
 		
@@ -93,27 +111,66 @@ class postgres_i implements db_connector
 	}
 	
 	function setbook(array $book){
-			
+		//ここに処理が来た時点で未登録書籍であること
+		$query = "INSERT INTO bookshelf(ISBN, URL, Image, Author, PubDate, Title, amount)";
+		$query .= "VALUES ('". $book->Items->Item->ItemAttributes->ISBN ."',";
+		$query .= "'". $book->Items->Item->DetailPageURL ."',";
+		$query .= "'". $book->Items->Item->LargeImage->URL ."',";
+		$query .= "'". $book->Items->Item->ItemAttributes->Author ."',";
+		$query .= "'". $book->Items->Item->ItemAttributes->PublicationDate ."',";
+		$query .= "'". $book->Items->Item->ItemAttributes->Title ."',";
+		$query .= "0);";
+		
+		pg_query($dbconn_book, $query);
+		return pg_last_error($dbconn_book);
+	}
+	
+	function addbook($isbn){
+		//ここに処理が来た時点で登録済みの書籍であること
+		$data = find($isbn);
+		//冊数加算
+		$data[6] ++;
+		
+		pg_query($dbconn_book, "UPDATE bookshelf SET amount={$data[6]} WHERE ISBN='{$isbn}';");
+		return pg_last_error($dbconn_book);
 	}
 	
 	function find($isbn){
+		$value = pg_query($dbconn_book, "SELECT * FROM bookshelf WHERE ISBN='{$isbn}';");
+		if(pg_num_rows($value) == 0) return 0;	//検索結果NULL？
 		
+		return pg_fetch_row($value, 0);
 	}
 	
 	function rmbook($isbn){
-		
+		pg_query($dbconn_book, "DELETE FROM bookshelf WHERE ISBN='{$isbn}';");
+		return pg_last_error($dbconn_book);
 	}
 	
 	function borrow($isbn, $user){
+		//ここに処理が来た時点で登録済みかつ、貸出中でない書籍であること
+		$query = "INSERT INTO borrows(ID, ISBN, bDate, rDate)";
+		$query .= "VALUES ('{$user}', '{$isbn}',";
+		$query .= "'". date("Y-m-d") ."', NULL);";
 		
+		pg_query($dbconn_borrow, $query);
+		return pg_last_error($dbconn_borrow);
 	}
 	
 	function repayment($isbn, $user){
-		
+		$rdate = date("Y-m-d");
+		pg_query($dbconn_book, "UPDATE borrows SET rDate='{$rdate}' WHERE ISBN='{$isbn}' AND ID='{$user}';");
+		return pg_last_error($dbconn_book);
 	}
 	
 	function lending(){
+		$value = pg_query($dbconn_borrow, "SELECT ID, ISBN, bDate FROM borrows WHERE rDate=NULL");
+		if(pg_num_rows($value) == 0) return 0;	//検索結果NULL？
 		
+		for($i=0; $i<pg_num_rows($value); $i++){
+			data[] = pg_fetch_row($value, $i);
+		}
+		return data;
 	}
 }
 
